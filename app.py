2b713552a212b7681089cfca01055244786db123
Mashkhurbek <mashkhurbek.mashrabov@gmail.com>
2024-06-07 06:52:45 +0500

    routing

--- app.py
+++ app.py
@@ -2,14 +2,40 @@ from webob import Request, Response
 
 
 class TurboHTTP:
+    def __init__(self):
+        self.routes = dict()
+
     def __call__(self, environ, start_response, *args, **kwargs):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
     def handle_request(self, request):
-        user_agent = request.headers.get("User-Agent", "Unknown")
         response = Response()
-        response.status_code = 200
-        response.text = f"Hello, {user_agent}!"
+
+        handler = self.find_handler(request)
+        if handler is not None:
+            handler(request, response)
+            response.status_code = 200
+        else:
+            response = self.default_response(response)
+
         return response
+
+    def find_handler(self, request):
+        try:
+            return self.routes[request.path]
+        except KeyError:
+            return None
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not Found"
+        return response
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from app import TurboHTTP
 
 app = TurboHTTP()
+
+
+@app.route("/")
+def home(request, response):
+    response.text = "Hello, World!"
+    return response
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "About Page"
+    return response
